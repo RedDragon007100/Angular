c3fb0052138636a5b562fdf187bb1b87417f0dfa
RedDragon007100 <roger.reddragon007@gmail.com>
2018-11-14 21:21:44 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCuI8au65_fvwJGpfU29D8Wp1GS0Km4IMY",
+    authDomain: "primer-proyecto-4780c.firebaseapp.com",
+    databaseURL: "https://primer-proyecto-4780c.firebaseio.com",
+    projectId: "primer-proyecto-4780c",
+    storageBucket: "primer-proyecto-4780c.appspot.com",
+    messagingSenderId: "732822841554"
+  }
 };
 
 /*
